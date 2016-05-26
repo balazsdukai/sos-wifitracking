5ef732f58b1e55d4f4be267843a9fcba8f60dbee
Matthie456 <thijssie2008@gmail.com>
2016-05-26 12:12:56 +0200

    Updated file

--- first insert statement.sql
+++ first insert statement.sql
@@ -5,7 +5,7 @@ from wifilog;
 
 -- Run from here
 insert into codespace
-select nextval('codespaceid_seq'), 'codespacetest';
+select nextval('codespaceid_seq'), 'tudelft';
 
 insert into featureofinteresttype
 select nextval('featureofinteresttypeid_seq'), 'http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint';
@@ -18,12 +18,15 @@ select nextval('unitid_seq'), 'mac address';
 
 -- If related feature is pointing to AP, how to specify nearest AP?
 insert into relatedfeature
-select nextval('relatedfeature_seq'), #
+select nextval('relatedfeature_seq'), #;
 
 -- I think this is correct
 insert into observationtype
 select nextval('observationtypeid_seq'), 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement';
 
--- Do we need to fill out name and description, and other assumptions?
-insert into procedure
-select nextval('procedureid_seq'), 'var', 1, 'eduroam_access_point', 1, 'name of procedure', 1, 'description', 'F', NULL, 'F'
+insert into "procedure"
+select nextval('procedureid_seq'), 'F', 1, 'wifi_access_point', 1, 'Scan', 1, 'WiFi Scan', 'F', 'F';
+
+insert into observableproperty
+select nextval('observablepropertyid_seq'), 'F', 'mac_address', 1, 'Mac', 1, 'Mac addresses scanned by wifi access point';
+
