795dcb33bc53151166298546d7cda1447f0bccf3
Matthie456 <thijssie2008@gmail.com>
2016-05-26 13:03:16 +0200

    Added other units and observableproperties

--- first insert statement.sql
+++ first insert statement.sql
@@ -16,6 +16,10 @@ select nextval('procdescformatid_seq'), 'http://www.opengis.net/sensorML/1.0.1';
 --need to update this with rssi, snr, netid
 insert into unit
 select nextval('unitid_seq'), 'mac address';
+insert into unit
+select nextval('unitid_seq'), 'db';
+insert into unit
+select nextval('unitid_seq'), 'netid';
 
 /*
 -- If related feature is pointing to AP, how to specify nearest AP?
@@ -32,4 +36,12 @@ select nextval('procedureid_seq'), 'F', 1, 'wifi_access_point', 1, 'Scan', 1, 'W
 
 insert into observableproperty
 select nextval('observablepropertyid_seq'), 'F', 'mac_address', 1, 'Mac', 1, 'Mac addresses scanned by wifi access point';
+insert into observableproperty
+select nextval('observablepropertyid_seq'), 'F', 'netid', 1, 'NetID', 1, 'NetID of user on device scanned by wifi access point';
+insert into observableproperty
+select nextval('observablepropertyid_seq'), 'F', 'rssi', 1, 'Received Signal Strenght Indicator', 1, 'RSSI of device scanned by wifi access point';
+insert into observableproperty
+select nextval('observablepropertyid_seq'), 'F', 'snr', 1, 'Signal to Noise Ratio', 1, 'SNR of device scanned by wifi access point';
 
+insert into offering
+select nextval('offeringid_seq'), 'F', 'wifi_access_points', 1, 'WiFi Access Points', 1, 'Network of all WiFi Access Points';
