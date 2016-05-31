4abfa13781b8ef907ebedf1275f2630e83dbcc5e
balazsdukai <balazs.dukai@gmail.com>
2016-05-31 12:28:50 +0200

    distinct apnames fix

--- second.sql
+++ second.sql
@@ -7,8 +7,17 @@ create table access_points
     maploc text,
     geom geometry
 );
+/*
 insert into access_points
 select t1.id, apname, t1.maploc, geometry
 from t1, buildings as bld
 where t1.id = bld.id;
 drop table t1;
+ */
+
+
+insert into access_points (apname, maploc, geom)
+         select distinct on (t1.apname) t1.apname, t1.maploc, geometry 
+         from t1, buildings
+         where t1.id = buildings.id; 
+         drop table t1;
